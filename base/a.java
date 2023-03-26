fd04295f3fd4341c929e7eb96d5c3c03a0a13dbc
gzy <6988577@qq.com>
2023-03-26 19:17:22 +0800

    first commit

---
+++ base/a.java
@@ -0,0 +1,21 @@
+package base;
+
+public class a
+{
+    public static void main(String[] args) {
+//        ctrl + /快速注释鼠标光标所在的一行
+//        ctrl + shift + /快速注释选中的块
+//        两个//输出一个/（可叠加）
+        System.out.println("\\hello\\");
+//        使用中文双引号
+        System.out.println("say:”输出引号“");
+        System.out.println("say:\"输出引号\"");
+//        \r!=\n 回车不是换行
+//          456
+        System.out.println("12345\r456");
+//        12345
+//          45
+        System.out.println("12345\r\n456");
+        //E:\PyCharm Community Edition 2022.2.3\pydemo\venv\Scripts\python.exe
+    }
+}
